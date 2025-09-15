7f6ba1475ff198d50df87b1328a8e6e4dc3a40ae
ColorlessSaber <nuns-pastes-0s@icloud.com>
2025-09-15 15:13:18 -0700

    Create two more web pages
    Made two more web pages--new recipe and cookbook. The next step is to start generating recipes and getting them to show up.

--- cookbook/recipes/urls.py
+++ cookbook/recipes/urls.py
@@ -6,4 +6,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='recipes-home'),
+    path('new-recipe/', views.new_item, name='new-recipe'),
+    path('cookbook/', views.cookbook, name='cookbook'),
 ]
--- cookbook/recipes/views.py
+++ cookbook/recipes/views.py
@@ -2,4 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    """
+    Generates the home page.
+
+    :param request:
+    :return:
+    """
     return render(request, 'recipes/recipes-home.html')
+
+def new_item(request):
+    """
+    Creates a new recipe.
+
+    :param request:
+    :return:
+    """
+    # TODO add in form logic for adding new recipe with ingredients.
+    return render(request, 'recipes/new-recipe.html')
+
+def cookbook(request):
+    """
+    Opens the webpage showing everything in the cookbook.
+
+    :param request:
+    :return:
+    """
+    return render(request, 'recipes/cookbook.html')
