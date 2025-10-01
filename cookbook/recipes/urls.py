154a7b850361c88110b9e18a25a9ab877e5d0c3d
ColorlessSaber <nuns-pastes-0s@icloud.com>
2025-10-01 15:23:26 -0700

    New web page for ingredient group
    Working on creating the backend work for the new web page for creating a new ingredient group--both for new and existing recipes.

--- cookbook/recipes/urls.py
+++ cookbook/recipes/urls.py
@@ -7,5 +7,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='recipes-home'),
     path('new-recipe/', views.new_item, name='new-recipe'),
+    path('recipe/<int:recipe_pk>/new-ingredient-group/', views.new_ingredient_group, name='new-ingredient-group'),
     path('cookbook/', views.cookbook, name='cookbook'),
 ]
--- cookbook/recipes/views.py
+++ cookbook/recipes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .forms import RecipesForm, IngredientsFormSet
 from .models import Recipes, IngredientGroup, Ingredients
 
@@ -20,14 +21,33 @@ def new_item(request):
     :return:
     """
     if request.method == "POST":
-        print("save form!")
-        return render(request, 'recipes/recipes-home.html') #TODO update later
+        recipe_form = RecipesForm(request.POST)
+        if recipe_form.is_valid():
+            new_recipe = recipe_form.save()
+            return redirect('new-ingredient-group', recipe_pk=new_recipe.pk)
+        else:
+            messages.error(request, 'Invalid form was submitted. Please try again.')
     else:
         recipe_form = RecipesForm()
-        context = {
-            'recipe_form': recipe_form,
-        }
-        return render(request, 'recipes/new-recipe.html', context)
+
+    context = {
+        'recipe_form': recipe_form,
+    }
+    return render(request, 'recipes/new-recipe.html', context)
+
+def new_ingredient_group(request, recipe_pk):
+    """
+    Creates a new ingredient group and associated ingredients, and links the group to the recipe.
+
+    This function is used adding new ingredient groups to existing and new recipes.
+
+    :param request:
+    :param recipe_pk: The primary key of the recipe that the new ingredient group will be associated with.
+    :return:
+    """
+    recipe = get_object_or_404(Recipes, pk=recipe_pk)
+    print(recipe_pk)
+    return render(request, 'recipes/cookbook.html')
 
 def cookbook(request):
     """
