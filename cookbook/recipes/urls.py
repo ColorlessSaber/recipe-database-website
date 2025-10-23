9c9931133fec6caaf9858323659c130b1741f7bf
ColorlessSaber <nuns-pastes-0s@icloud.com>
2025-10-23 15:35:07 -0700

    edit, new, and delete ingredient group
    Completed adding the ability to edit and delete existing ingredient group(s), and create a new ingredient group for a selected recipe.

--- cookbook/recipes/urls.py
+++ cookbook/recipes/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     path('', views.home, name='recipes-home'),
     path('new-recipe/', views.new_recipe, name='new-recipe'),
     path('cookbook/', views.cookbook, name='cookbook'),
-    path('recipe/<int:recipe_id>/', views.recipe_details, name='recipe-details'),
+    path('recipe/<int:recipe_id>/details', views.recipe_details, name='recipe-details'),
     path('recipe/<int:recipe_id>/edit/', views.edit_recipe, name='edit-recipe'),
+    path('recipe/<int:recipe_id>/new-ingredient-group/', views.new_ingredient_group, name='new-ingredient-group'),
+    path('recipe/<int:recipe_id>/<int:ingredient_group_id>/edit/', views.edit_ingredient_group, name='edit-ingredient-group'),
     path('recipe/<int:recipe_id>/delete/', views.delete_recipe, name='delete-recipe'),
+    path('recipe/<int:recipe_id>/<int:ingredient_group_id>/delete/', views.delete_ingredient_group, name='delete-ingredient-group'),
 ]
--- cookbook/recipes/views.py
+++ cookbook/recipes/views.py
@@ -38,7 +38,7 @@ def new_recipe(request):
                     ingredient_formset.instance = ingredient_group
                     ingredient_formset.save()
 
-                messages.success(request, f'Recipe {recipe.name} has been successfully saved!')
+                messages.success(request, f'Recipe "{recipe.name}" has been successfully saved!')
                 return redirect("recipes-home")
             except Exception as e:
                 messages.error(request, f'There was an error when saving recipe: {e}')
@@ -104,15 +104,95 @@ def edit_recipe(request, recipe_id):
     recipe = get_object_or_404(Recipes, id=recipe_id)
     if request.method == "POST":
         recipe_form = RecipesForm(request.POST, prefix="recipe_form", instance=recipe)
-        recipe = recipe_form.save()
-        messages.success(request, f'Recipe {recipe.name} has been successfully saved!')
-        return redirect('recipe-details', recipe_id=recipe_id)
+
+        if recipe_form.is_valid():
+            recipe = recipe_form.save()
+            messages.success(request, f'Recipe "{recipe.name}" has been successfully saved!')
+            return redirect('recipe-details', recipe_id=recipe_id)
+        else:
+            messages.error(request, 'Invalid form was submitted. Please try again.')
     else:
         recipe_form = RecipesForm(instance=recipe, prefix="recipe_form")
-        return render(request, 'recipes/edit-recipe.html', {
-            'recipe': recipe,
-            'recipe_form': recipe_form,
-        })
+    return render(request, 'recipes/edit-recipe.html', {
+        'recipe': recipe,
+        'recipe_form': recipe_form,
+    })
+
+def edit_ingredient_group(request, ingredient_group_id, recipe_id):
+    """
+    Opens webpage to allow user to edit the selected ingredient group.
+
+    :param request:
+    :param ingredient_group_id: The id of the ingredient group user wishes to edit
+    :param recipe_id: The id of the recipe the ingredient group is associated with
+    :return:
+    """
+    ingredient_group = get_object_or_404(IngredientGroup, id=ingredient_group_id)
+    recipe = get_object_or_404(Recipes, id=recipe_id)
+    if request.method == "POST":
+        ingredient_group_form = IngredientGroupForm(request.POST, instance=ingredient_group, prefix="ingredient_group_form")
+        ingredient_formset = IngredientsFormSet(request.POST, instance=ingredient_group, prefix="ingredient_formset")
+
+        if ingredient_group_form.is_valid() and ingredient_formset.is_valid():
+            ingredient_group = ingredient_group_form.save()
+            ingredient_formset.instance = ingredient_group
+            ingredient_formset.save()
+
+            messages.success(request, f'Ingredient Group "{ingredient_group.name}" has been successfully saved!')
+            return redirect('recipe-details', recipe_id=recipe_id)
+        else:
+            messages.error(request, 'Invalid form was submitted. Please try again.')
+
+    else:
+        ingredient_group_form = IngredientGroupForm(instance=ingredient_group, prefix="ingredient_group_form")
+        ingredient_formset = IngredientsFormSet(instance=ingredient_group, prefix="ingredient_formset")
+
+    return render(request, 'recipes/ingredient-group.html', {
+        'ingredient_group_form': ingredient_group_form,
+        'ingredient_formset': ingredient_formset,
+        'recipe': recipe,
+        'edit_html': True,
+    })
+
+def new_ingredient_group(request, recipe_id):
+    """
+    Creates a new ingredient group for selected recipe.
+
+    :param request:
+    :param recipe_id: the id of the recipe the new ingredient group will be created for.
+    :return:
+    """
+    recipe = get_object_or_404(Recipes, id=recipe_id)
+    if request.method == "POST":
+        ingredient_group_form = IngredientGroupForm(request.POST, prefix="ingredient_group_form")
+        ingredient_formset = IngredientsFormSet(request.POST, prefix="ingredient_formset")
+
+        if ingredient_formset.is_valid() and ingredient_group_form.is_valid():
+            try:
+                with transaction.atomic():
+                    ingredient_group_form.instance.recipe_ref = recipe
+                    ingredient_group = ingredient_group_form.save()
+
+                    ingredient_formset.instance = ingredient_group
+                    ingredient_formset.save()
+
+                messages.success(request, f'Ingredient Group "{ingredient_group.name}" has been successfully saved!')
+                return redirect('recipe-details', recipe_id=recipe_id)
+            except Exception as e:
+                messages.error(request, f'There was an error when saving ingredient group: {e}')
+        else:
+            messages.error(request, 'Invalid form was submitted. Please try again.')
+
+    else:
+        ingredient_group_form = IngredientGroupForm(prefix="ingredient_group_form")
+        ingredient_formset = IngredientsFormSet(prefix="ingredient_formset")
+
+    return render(request, 'recipes/ingredient-group.html', {
+        'ingredient_group_form': ingredient_group_form,
+        'ingredient_formset': ingredient_formset,
+        'recipe': recipe,
+        'edit_html': False,
+    })
 
 def delete_recipe(request, recipe_id):
     """
@@ -124,5 +204,27 @@ def delete_recipe(request, recipe_id):
     """
     recipe = get_object_or_404(Recipes, id=recipe_id)
     recipe.delete()
-    messages.success(request, f'Recipe {recipe.name} has been successfully deleted!')
+    messages.success(request, f'Recipe "{recipe.name}" has been successfully deleted!')
     return redirect('recipes-home')
+
+def delete_ingredient_group(request, recipe_id, ingredient_group_id):
+    """
+    Deletes an individual ingredient group for the recipe from the database,
+    as long as there is at least one or more ingredient groups for the recipe.
+
+    :param request:
+    :param recipe_id: The id of the recipe the ingredient group is associated with
+    :param ingredient_group_id: the id of the ingredient group user wishes to delete
+    :return:
+    """
+
+    recipe = get_object_or_404(Recipes, id=recipe_id)
+    ingredient_group = get_object_or_404(IngredientGroup, id=ingredient_group_id)
+    number_of_ingredient_groups = len(recipe.ingredient_groups.all())
+    if number_of_ingredient_groups > 1:
+        ingredient_group.delete()
+        messages.success(request, f'Ingredient Group "{ingredient_group.name}" has been successfully deleted!')
+        return redirect('recipe-details', recipe_id=recipe_id)
+    else:
+        messages.error(request, 'Operation cancelled! A recipe needs at lest one ingredient group.')
+    return redirect('recipe-details', recipe_id=recipe_id)
