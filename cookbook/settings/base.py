01eaa017fe03daa4af8919de956eceafcec21e6f
ColorlessSaber <nuns-pastes-0s@icloud.com>
2025-10-28 16:36:19 -0700

    unnested the Django website
    Made the decision to unnest the Django website. Reason why is it helps with making the Docker container easier, and it matches how most online examples creat theirs.

--- cookbook/settings/base.py
+++ cookbook/settings/base.py
@@ -7,6 +7,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
+# 'True' for development; 'False' for production
 DEBUG = env.bool('DJANGO_DEBUG', False)
 DATABASES = {
     'default': env.db()
